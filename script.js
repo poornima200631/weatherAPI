913aca0758b144f1bd23aba57cdf52f563e11a0b
poornima200631 <poornimatewatia48@gmail.com>
2025-06-19 18:13:38 +0530

    Added new code

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 async function getWeather() {
     event.preventDefault();
+
     try {
       const place = document.getElementById("search").value;
       const apiKey = "31387fbb9b6c465582264237251906";  
+      if(!place){
+        document.getElementById('city').innerText = "City cannot be empty❗️❗️";
+        document.getElementById('temperature').textContent = "--";
+        document.getElementById('humidity').textContent = "--";
+        document.getElementById('wind').textContent = "--";
+        return;
+      }
   
       const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${place}`);
       const data = await response.json();
@@ -10,9 +18,8 @@ async function getWeather() {
       document.getElementById('temperature').textContent = ` ${data.current.temp_c}°C`;
       document.getElementById('humidity').textContent = ` ${data.current.humidity}%`;
       document.getElementById('wind').textContent = ` ${data.current.wind_kph} kph`;
-      console.log("Your Input:", place);
-      console.log("API Returned:", data.location.name, data.current.temp_c);
-
+      document.getElementById('city').textContent=`${place}`;
+      document.getElementById("search").value = "";
   
     } catch (error) {
       console.log("Something went wrong:", error);
